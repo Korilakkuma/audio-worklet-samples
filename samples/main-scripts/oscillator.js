6cdbc713f8ec634a1b979c388ba3282c5637ec4c
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-12-13 16:10:00 +0900

    Refactor oscillator (use `AudioParamDescriptor`)

--- samples/main-scripts/oscillator.js
+++ samples/main-scripts/oscillator.js
@@ -2,49 +2,52 @@
 
 const context = new AudioContext();
 
-document.addEventListener('DOMContentLoaded', () => {
-    let type      = 'sine';
-    let frequency = 440;
+const promise = context.audioWorklet.addModule('./worklet-scripts/oscillator.js');
 
-    context.audioWorklet.addModule('./worklet-scripts/oscillator.js').then(() => {
-        const oscillator = new AudioWorkletNode(context, 'oscillator');
+promise
+  .then(() => {
+    const oscillator = new AudioWorkletNode(context, 'oscillator');
 
-        document.querySelector('button').addEventListener('click', async (event) => {
-            if (context.state !== 'running') {
-                await context.resume();
-            }
+    document.querySelector('button').addEventListener('click', async (event) => {
+      if (context.state !== 'running') {
+        await context.resume();
+      }
 
-            const button = event.target;
+      const button = event.target;
 
-            if (button.textContent === 'START') {
-                oscillator.connect(context.destination);
+      if (button.textContent === 'START') {
+        oscillator.connect(context.destination);
 
-                button.textContent = 'STOP';
-            } else {
-                oscillator.disconnect(0);
+        button.textContent = 'STOP';
+      } else {
+        oscillator.disconnect(0);
 
-                button.textContent = 'START';
-            }
-        }, false);
+        button.textContent = 'START';
+      }
+    }, false);
 
-        document.querySelector('form').addEventListener('change', event => {
-            const form = event.currentTarget;
+    document.querySelector('form').addEventListener('change', (event) => {
+      const form = event.currentTarget;
 
-            for (let i = 0, len = form.elements['radio-wave-type'].length; i < len; i++) {
-                if (form.elements['radio-wave-type'][i].checked) {
-                    type = form.elements['radio-wave-type'][i].value;
+      for (let i = 0, len = form.elements['radio-wave-type'].length; i < len; i++) {
+        if (form.elements['radio-wave-type'][i].checked) {
+          const type = form.elements['radio-wave-type'][i].value;
 
-                    oscillator.port.postMessage({ type });
+          oscillator.port.postMessage(type);
 
-                    break;
-                }
-            }
-        }, false);
+          break;
+        }
+      }
+    }, false);
 
-        document.querySelector('[type="number"]').addEventListener('input', event => {
-            frequency = event.currentTarget.valueAsNumber;
+    document.querySelector('[type="range"]').addEventListener('change', (event) => {
+      const frequency = event.currentTarget.valueAsNumber;
 
-            oscillator.port.postMessage({ frequency });
-        }, false);
-    });
-}, false);
+      const currentTime = context.currentTime;
+      const audioParam  = oscillator.parameters.get('frequency');
+
+      audioParam.setValueAtTime(audioParam.value, currentTime);
+      audioParam.linearRampToValueAtTime(frequency, currentTime + 0.5);
+    }, false);
+  })
+  .catch(console.error);
